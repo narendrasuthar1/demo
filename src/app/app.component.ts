d60e5ac290a429f1184fc9b5ab3ddfe1245cd905
Rounak Suthar <rounaksolanki37@gmail.com>
2023-05-18 01:44:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-demo-app';
+
+  title :any;
+  headers:HttpHeaders ;
+
+
+  constructor(private http: HttpClient) {
+    this.headers= new HttpHeaders()
+      .set('Content-type', 'application/json')
+      .set('X-Fab-Api-Key','Fabzia@#009');
+  }
+
+
+
+  login() {
+    const inputData = {
+      mobile : 9321103771
+    };
+    this.checkLoginAndGetOtp(inputData).subscribe(res=>{
+      this.title= res;
+    });
+  }
+
+    checkLoginAndGetOtp(inputData:any): Observable<any> {
+      return this.http.post('/api/login',inputData,{ 'headers': this.headers })
+    }
 }
